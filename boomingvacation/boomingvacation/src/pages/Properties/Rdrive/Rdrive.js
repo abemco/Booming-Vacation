1574eed56d9a49983977a4f07536e3f882561406
Jose Araujo <JoseAraujoBH@gmail.com>
2018-07-24 21:17:14 -0400

    add photos to romeo drive

--- boomingvacation/boomingvacation/src/pages/Properties/Rdrive/Rdrive.js
+++ boomingvacation/boomingvacation/src/pages/Properties/Rdrive/Rdrive.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import bv15 from './BV15.jpg'
+import rd1 from './RD1.JPG'
+import rd2 from './RD2.JPG'
+import rd3 from './RD3.JPG'
+import rd4 from './RD4.JPG'
+import rd5 from './RD5.JPG'
+import rd6 from './RD6.JPG'
 
 import { Container, Row, Col } from 'reactstrap';
 
@@ -11,10 +16,10 @@ class Example extends React.Component {
         <Container>
 
             <Row>
-                <Col size="md-3"><img src={bv15}></img></Col>
-                <Col size="md-3"><img src={bv15}></img></Col>
-                <Col size="md-3"><img src={bv15}></img></Col>
-                <Col size="md-3"><img src={bv15}></img></Col>
+                <Col size="md-3"><img src={rd1}></img></Col>
+                <Col size="md-3"><img src={rd2}></img></Col>
+                <Col size="md-3"><img src={rd3}></img></Col>
+                
                 {/* <Col size="sm-2"><img src={bv15}></img></Col>
                 <Col size="sm-2"><img src={bv15}></img></Col> */}
             </Row>
