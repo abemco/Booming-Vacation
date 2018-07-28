42e4440c078486d66fce1b62e3dc59b9497e9caf
Jose Araujo <JoseAraujoBH@gmail.com>
2018-07-27 21:07:16 -0400

    resize some images

--- boomingvacation/boomingvacation/src/pages/Properties/Wind23/Wind23.js
+++ boomingvacation/boomingvacation/src/pages/Properties/Wind23/Wind23.js
@@ -6,6 +6,8 @@ import React from 'react';
 
 import { Container, Row, Col } from 'reactstrap';
 
+const divStyles = { paddingTop: "30px" };
+
 class Example extends React.Component {
     render () {
         return (
@@ -21,9 +23,31 @@ class Example extends React.Component {
                 <Col size="sm-2"><img src={bv15}></img></Col> */}
             </Row>
                 
+            <div style={divStyles}>
             <Row>
-            <Col></Col>
+            <Col> 
+            
+                <p>Storey Lake Resort is situated in Kissimmee. It features a garden, pool views and free WiFi.
+
+                The holiday home is equipped with a satellite flat-screen TV. The kitchen is fitted with a dishwasher and an oven, as well as coffee machine.
+
+                The holiday home offers an outdoor pool. Guests can also relax in the shared lounge area.
+
+                The Villages is 11.2 miles from Storey Lake Resort. The nearest airport is Orlando Sanford International Airport, 35.4 miles from the accommodation.</p>
+                
+            <ul>
+              <li><i class="fas fa-bed"></i>    4 Bedrooms</li>
+              <li><i class="fas fa-shower"></i>    4 Bathrooms</li>
+              <li><i class="fas fa-users"></i>    12 Guests</li>
+              <li><i class="fas fa-swimmer"></i>    Private Pool</li>
+              <li><i class="fas fa-wifi"></i>    Free High Speed WiFi</li>
+              <li><i class="fas fa-dumbbell"></i>    Fitness Center</li>
+              <li><i class="fas fa-swimming-pool"></i>    On-site Water Park</li>
+            </ul>
+
+            </Col>
             </Row>
+            </div>
 
         </Container>
         );
--- boomingvacation/boomingvacation/src/pages/Properties/Wind26/Wind26.js
+++ boomingvacation/boomingvacation/src/pages/Properties/Wind26/Wind26.js
@@ -1,3 +1,15 @@
+import React from 'react';
+
+// import w13 from './W13.jpg'
+// import w14 from './W14.jpg'
+// import w15 from './W15.jpg'
+// import w16 from './W16.jpg'
+// import w17 from './W17.jpg'
+
+import { Container, Row, Col } from 'reactstrap';
+
+const divStyles = { paddingTop: "30px" };
+
 class Example extends React.Component {
     render () {
         return (
@@ -5,17 +17,39 @@ class Example extends React.Component {
         <Container>
 
             <Row>
-                {/* <Col size="md-3"><img src={w4}></img></Col>
-                <Col size="md-3"><img src={w5}></img></Col>
-                <Col size="md-3"><img src={w6}></img></Col> */}
+                {/* <Col size="md-3"><img src={w13}></img></Col>
+                <Col size="md-3"><img src={w14}></img></Col>
+                <Col size="md-3"><img src={w15}></img></Col> */}
                 
-                {/* <Col size="sm-2"><img src={bv15}></img></Col>
-                <Col size="sm-2"><img src={bv15}></img></Col> */}
+                {/* <Col size="sm-2"><img src={bv16}></img></Col>
+                <Col size="sm-2"><img src={bv17}></img></Col> */}
             </Row>
                 
+            <div style={divStyles}>
             <Row>
-            <Col>.col</Col>
+            <Col> 
+            
+                <p>Storey Lake Resort is situated in Kissimmee. It features a garden, pool views and free WiFi.
+
+                The holiday home is equipped with a satellite flat-screen TV. The kitchen is fitted with a dishwasher and an oven, as well as coffee machine.
+
+                The holiday home offers an outdoor pool. Guests can also relax in the shared lounge area.
+
+                The Villages is 11.2 miles from Storey Lake Resort. The nearest airport is Orlando Sanford International Airport, 35.4 miles from the accommodation.</p>
+                
+            <ul>
+              <li><i class="fas fa-bed"></i>    4 Bedrooms</li>
+              <li><i class="fas fa-shower"></i>    4 Bathrooms</li>
+              <li><i class="fas fa-users"></i>    12 Guests</li>
+              <li><i class="fas fa-swimmer"></i>    Private Pool</li>
+              <li><i class="fas fa-wifi"></i>    Free High Speed WiFi</li>
+              <li><i class="fas fa-dumbbell"></i>    Fitness Center</li>
+              <li><i class="fas fa-swimming-pool"></i>    On-site Water Park</li>
+            </ul>
+
+            </Col>
             </Row>
+            </div>
 
         </Container>
         );
